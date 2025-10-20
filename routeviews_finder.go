4972f051813b0c0cf4b045d0cc8b37d333e2fd5a
Shane Alcock <shane@alcock.co.nz>
2025-10-20 18:49:18 -0400

    Add override to handle queries that explicitly use route-views2.saopaulo

--- routeviews_finder.go
+++ routeviews_finder.go
@@ -153,6 +153,7 @@ func (f *RouteViewsFinder) getCollectorURL(collector Collector) string {
 	// but for route-views2, the url is https://archive.routeviews.org/bgpdata/
 	CollectorNameOverride := map[string]string{
 		"route-views2": "",
+		"route-views2.saopaulo": "ix-br2.gru",
 	}
 
 	if override, exists := CollectorNameOverride[collector.Name]; exists {
